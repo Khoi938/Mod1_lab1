6c51e35bfaa38abc4f71b9dd5ddb47786f607bd6
Khoi Le <eclipse938@gmail.com>
2018-11-22 06:21:32 -0500

    just testing

---
+++ Mod1_lab1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod1_lab1
+{
+    class Class1
+    {
+        static void Second(string [] args)
+        {
+            Console.WriteLine("hello");
+        }
+        
+    }
+}
---
+++ Mod1_lab1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mod1_lab1
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool result = true;
+            if (result)
+                Console.WriteLine("Result is true");
+                Console.WriteLine("Will this print as part of the if statement?");
+            Console.WriteLine("Does this print at all?");
+            //string first;
+            //string last;
+            //string bDay;
+            //string address;
+            //string city;
+            //string state;
+
+            //Console.WriteLine("Hello! What is your name?");
+            //first = Console.ReadLine();
+            //Console.WriteLine($"Hello {first}");
+        }
+        static void ain(string[] args)
+        {
+            string firstName = "";
+            string lastName = "";
+            string street = "";
+            int age = 0;
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            firstName = "Khoi";
+            lastName = "le";
+            age = 33;
+            city = "Tampa";
+            country = "USA";
+            street = "4721 Town N Country Blvd";
+            birthDate = new DateTime(1986, 4, 6);
+            var x = birthDate.ToShortDateString();
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            Console.WriteLine("I'm {0} year old.",age);
+            Console.WriteLine("I live on, " + street + ", " + city + ", " + country);
+
+            Console.WriteLine($"I was born on {x}");
+            Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
